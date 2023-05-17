b5006621c5d09f4f8f5f7e1b5aaea532f7893075
kushida.tomohiro <falsity_0330@yahoo.co.jp>
2023-05-17 12:17:53 +0900

    Blade　バリデーションメッセージ表示

--- laravel-practis/resources/views/companies/edit.blade.php
+++ laravel-practis/resources/views/companies/edit.blade.php
@@ -5,7 +5,9 @@
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <div class="container">
         <div class="row justify-content-center">
+
             <div class="col-md-8">
+                @include('companies/message')
                 <div class="card">
                     <div class="card-header">会社情報　編集</div>
                     <div class="card-body">
