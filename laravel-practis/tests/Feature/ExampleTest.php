e56bb59660915554d3a39ed98ef1367bd188b6e2
kushida.tomohiro <falsity_0330@yahoo.co.jp>
2023-05-19 11:42:24 +0900

    テスト実行

--- laravel-practis/tests/Feature/ExampleTest.php
+++ laravel-practis/tests/Feature/ExampleTest.php
@@ -7,12 +7,9 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_お試し(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('/login');
 
         $response->assertStatus(200);
     }
