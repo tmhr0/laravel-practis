09aeb993149b8cd1faea73dd7b08b69e2e59f08c
kushida.tomohiro <falsity_0330@yahoo.co.jp>
2023-05-19 12:10:38 +0900

    指定ページの文字列調査実行

--- laravel-practis/tests/Feature/ExampleTest.php
+++ laravel-practis/tests/Feature/ExampleTest.php
@@ -4,7 +4,7 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
-
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 class ExampleTest extends TestCase
 {
     public function test_お試し(): void
@@ -12,5 +12,14 @@ class ExampleTest extends TestCase
         $response = $this->get('/login');
 
         $response->assertStatus(200);
+    }
+    public function test_login_view_text_match(): void
+    {
+        $response = $this->get(route('login'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('auth.login')
+            ->assertSee('Remember me')
+            ->assertSee('Forgot your password?');
     }
 }
