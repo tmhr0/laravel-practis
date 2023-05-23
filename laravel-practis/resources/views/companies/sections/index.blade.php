eb4cc1813f22bf82baa9b7a058cd7168bfac0284
kushida.tomohiro <falsity_0330@yahoo.co.jp>
2023-05-23 15:56:32 +0900

    一覧ページ実装

---
+++ laravel-practis/resources/views/companies/sections/index.blade.php
@@ -0,0 +1,39 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+      integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <div class="container">
+            <div class="card">
+                <h1 class="card-header">部署情報 一覧ページ</h1>
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>会社名</th>
+                        <th>作成日</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @if(isset($sections))
+                        @foreach ($sections as $section)
+                            <tr>
+                                <td>{{ $section->id }}</td>
+                                <td>{{ $section->name }}</td>
+                                <td>{{ $section->created_at }}</td>
+                                <td>
+                                    <button type="button" class="btn btn-primary"
+                                            onclick="location.href='{{ route('companies.create') }}'">
+                                        {{ __('追加') }}
+                                    </button>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    @endif
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
