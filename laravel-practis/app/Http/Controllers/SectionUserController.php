a7fa497a13c883b28d883b37026252c92f21c75a
kushida.tomohiro <falsity_0330@yahoo.co.jp>
2023-05-24 15:00:13 +0900

    ユーザーの部署登録ページ　ルーティング設定

---
+++ laravel-practis/app/Http/Controllers/SectionUserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Company;
+use App\Models\Section;
+use App\Models\User;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+
+class SectionUserController extends Controller
+{
+    public function create(): View
+    {
+
+        return view('companies.sections.users.create ');
+    }
+}
---
+++ laravel-practis/resources/views/companies/sections/users/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">部署情報　登録</div>
+                    <div class="card-body">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
