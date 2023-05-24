8e59239b1722dcbc43bda85dd9e04c6bf336cf24
kushida.tomohiro <falsity_0330@yahoo.co.jp>
2023-05-24 19:29:41 +0900

    会社　重複登録　バリデーション

--- laravel-practis/app/Http/Requests/StoreCompanyRequest.php
+++ laravel-practis/app/Http/Requests/StoreCompanyRequest.php
@@ -22,7 +22,7 @@ class StoreCompanyRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:30'],
+            'name' => ['required', 'string', 'max:30', 'unique:sections'],
         ];
     }
 }
